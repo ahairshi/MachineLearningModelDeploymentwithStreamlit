9a984fcd49cbf0a6e36a55e6429db40d9f26132f
ahairshi <daisyfuentesahamed@gmail.com>
2023-10-16 17:33:43 +0000

    Changes in gitpod config file

--- 06_data_display_elements/main.py
+++ 06_data_display_elements/main.py
@@ -8,4 +8,8 @@ st.write(df)
 
 st.table(df)
 
-st.metric(label="Expenses", value=900, delta=20, delta_color="inverse")
+st.metric(label="Expenses", value=900, delta=20, delta_color="normal")
+st.metric(label="Expenses", value=900, delta=-20, delta_color="inverse")
+
+st.metric(label="Population", value=10000, delta=20, delta_color="normal")
+st.metric(label="Population", value=9900, delta=-100, delta_color="normal")
